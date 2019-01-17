aebdcd74c93e791b0c59e41258b8918b043b1a0b
Cesar Lizarraga <calizarr@gmail.com>
2019-01-16 22:29:31 -0600

    Add projectile function to discover projects in directory

--- modules/projects/projectile-conf.el
+++ modules/projects/projectile-conf.el
@@ -1,6 +1,27 @@
+
 (use-package projectile
   :demand
   :init (setq projectile-use-git-grep t)
   :config (projectile-global-mode t)
   :bind (("s-f" . projectile-find-file)
          ("s-F" . projectile-grep)))
+
+
+(defun projectile-discover-projects-in-directory (directory)
+  "Discover any projects in DIRECTORY and add them to the projectile cache.
+This function is not recursive and only adds projects with roots
+at the top level of DIRECTORY."
+  (interactive
+   (list (read-directory-name "Starting directory: ")))
+  (let ((subdirs (directory-files directory t)))
+    (mapcar
+     (lambda (dir)
+       (when (and (file-directory-p dir)
+                  (not (member (file-name-nondirectory dir) '(".." "."))))
+         (let ((default-directory dir)
+               (projectile-cached-project-root dir))
+           (when (projectile-project-p)
+             (projectile-add-known-project (projectile-project-root))))))
+     subdirs)))
+
+(push "*.mypy_cache" projectile-globally-ignored-directories)
