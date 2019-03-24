ede1167b0125674a3d8fe59f69d60d6a85901965
Cesar Lizarraga <calizarr@gmail.com>
2019-03-24 12:26:13 -0500

    Remove color highlighting for now

--- init.el
+++ init.el
@@ -59,13 +59,13 @@
 (load "http/http-init")
 (load "infrastructure/infra-init.el")
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-scrollbar-bg ((t (:background "#45bb4ed351db"))))
- '(company-scrollbar-fg ((t (:background "#39f441834408"))))
- '(company-tooltip ((t (:inherit default :background "#32e339873bbd"))))
- '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
- '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(company-scrollbar-bg ((t (:background "#45bb4ed351db"))))
+;;  '(company-scrollbar-fg ((t (:background "#39f441834408"))))
+;;  '(company-tooltip ((t (:inherit default :background "#32e339873bbd"))))
+;;  '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+;;  '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
--- modules/completion/company-conf.el
+++ modules/completion/company-conf.el
@@ -37,13 +37,13 @@
 
 (use-package color)
 
-(let ((bg (face-attribute 'default :background)))
-  (custom-set-faces
-   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
+;; (let ((bg (face-attribute 'default :background)))
+;;   (custom-set-faces
+;;    `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+;;    `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+;;    `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+;;    `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+;;    `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
 
 (autoload 'helm-company "helm-company") ;; Not necessary if using ELPA package
 (eval-after-load 'company
