b806aa4e5a80a6ce2983a877ffab393ff7fe997b
Cesar Lizarraga <calizarr@gmail.com>
2019-04-23 01:25:13 -0500

    Add comments as well as previous arguments in Windows

--- modules/languages/scala/metals-conf.el
+++ modules/languages/scala/metals-conf.el
@@ -30,10 +30,14 @@
 ;; (setq 'lsp-print-io t)
 
 ;; Install Metals Binary if not existing
+;; I also had previously:
+;; "--java-opt \"-XX:+UseG1GC\""
+;; "--java-opt \"-XX:+UseStringDeduplication\""
 
 (defun install-metals ()
   "This will install metals hopefully easily."
   (let
+      ;; Define the arguments to coursier
       ((args (string-join '(
                             "--java-opt \"-Xss4m\""
                             "--java-opt \"-Xms100m\""
@@ -42,16 +46,21 @@
                             "-r bintray:scalacenter/releases"
                             "-r sonatype:snapshots"
                             "-o /usr/local/bin/metals-emacs -f") " "))
+       ;; The java command to run the coursier download (OS agnostic)
        (command "java -noverify -jar coursier bootstrap"))
+    ;; Check if it already exists.
     (if (not (file-exists-p "/usr/local/bin/metals-emacs"))
+        ;; Check if we're not in Windows
         (if (not (string-equal system-type "windows-nt"))
             (progn
               (shell-command (format "bash -c %s" (shell-quote-argument "curl -L -o coursier https://git.io/coursier")))
               (shell-command (format "bash -c %s" (shell-quote-argument "chmod +x coursier")))
               (shell-command (format "bash -c %s" (shell-quote-argument (concat command " " args)))))
+          ;; If we're in Windows we use Powershell
           (progn
             (shell-command (format "powershell %s" "curl -o coursier https://git.io/coursier"))
             (shell-command (concat command " " args))))
+      ;; Otherwise, we don't do anything if it already exists
       (message "metals-emacs binary already exists"))))
 
 (install-metals)
