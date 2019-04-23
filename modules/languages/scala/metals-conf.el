4232f5cb25888bdd62e305f03f773d263467707e
Cesar Lizarraga <calizarr@gmail.com>
2019-04-22 19:05:43 -0500

    Add install-metals function to metals-conf.el

--- modules/languages/scala/metals-conf.el
+++ modules/languages/scala/metals-conf.el
@@ -29,3 +29,26 @@
 ;; Printing server I/O for debugging
 ;; (setq 'lsp-print-io t)
 
+;; Install Metals Binary if not existing
+
+(defun install-metals ()
+  (if (not (file-exists-p "/usr/local/bin/metals-emacs"))
+      (if (not (string-equal system-type "windows-nt"))
+          (progn
+            (shell-command (format "bash -c %s" (shell-quote-argument "curl -L -o coursier https://git.io/coursier")))
+            (shell-command (format "bash -c %s" (shell-quote-argument "chmod +x coursier")))
+            (shell-command (format "bash -c %s" (shell-quote-argument "
+./coursier bootstrap \
+  --java-opt -Xss4m \
+  --java-opt -Xms100m \
+  --java-opt -Dmetals.client=emacs \
+  org.scalameta:metals_2.12:0.5.0 \
+  -r bintray:scalacenter/releases \
+  -r sonatype:snapshots \
+  -o /usr/local/bin/metals-emacs -f"))))
+        (progn
+          (shell-command "curl -o coursier https://git.io/coursier")
+          (shell-command "java -noverify -jar coursier bootstrap --java-opt -Xss4m --java-opt -Xms100m --java-opt -Dmetals.client=emacs org.scalameta:metals_2.12:0.5.0 -r bintray:scalacenter/releases -r sonatype:snapshots -o /usr/local/bin/metals-emacs -f")))
+    (message "metals-emacs binary already exists")))
+
+(install-metals)
