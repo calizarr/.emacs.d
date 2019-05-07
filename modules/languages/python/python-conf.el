3e0ebf90c630fdbaae22a054962d60d4b060b1c0
Cesar Lizarraga <calizarr@gmail.com>
2019-05-07 08:11:55 -0500

    Add windows specific python-conf changes for use with conda
    Also overrode the conda--get-path-prefix function because it was
    using the old method of activating conda which accidentally adds
    warnings in the path

--- modules/languages/python/python-conf.el
+++ modules/languages/python/python-conf.el
@@ -19,3 +19,16 @@
 (use-package pyenv-mode
   :ensure)
 
+(if (is-windows)
+    (use-package conda
+      :ensure t
+      :init
+      (let (conda-root (file-name-directory (directory-file-name (file-name-directory (getenv "CONDA_EXE")))))
+        (setq conda-anaconda-home (getenv "_CONDA_ROOT")))
+      (defun conda--get-path-prefix (env-dir)
+        "Get a platform-specific path string to utilize the conda env in ENV-DIR.
+It's platform specific in that it uses the platform's native path separator."
+        (string-trim (shell-command-to-string (format "conda activate \"%s\"" env-dir))))
+      :config
+      (conda-env-initialize-interactive-shells)
+      (conda-env-initialize-eshell)))
