1821d9f243380927c570f2e18ca2226593edb9d1
Cesar Lizarraga <calizarr@gmail.com>
2019-06-11 18:42:44 -0500

    Remove scalafmt code hook

--- modules/languages/scala/scala-init.el
+++ modules/languages/scala/scala-init.el
@@ -1,5 +1,5 @@
 ;; Scala-mode, sbt-mode, ensime-mode settings for Scala IDE also has some globals
-(load "scalafmt-conf.el")
+;; (load "scalafmt-conf.el")
 (load "scala-mode-conf.el")
 (load "metals-conf.el")
 ;; (load "ensime-conf.el")
