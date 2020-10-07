75f9b4b8207f285659b562615bde1ef2dee37f88
Cesar Lizarraga <calizarr@gmail.com>
2020-10-07 17:12:25 -0500

    Make ylsp-operations function to not repeat code

--- modules/languages/lsp/lsp-conf.el
+++ modules/languages/lsp/lsp-conf.el
@@ -99,34 +99,29 @@
 
 (defvar ylsp-modeline "# yaml-language-server: $schema=")
 
+(defun ylsp-operations (modeline)
+  (save-excursion
+    (save-restriction
+      (widen)
+      (goto-char (point-min))
+      (insert modeline "\n"))))
+
 (defun ylsp-kustomize-modeline ()
   "Inserts kustomization modeline at top of buffer"
   (interactive)
   (let ((modeline (concat ylsp-modeline "https://json.schemastore.org/kustomization\n")))
-    (save-excursion
-      (save-restriction
-        (widen)
-        (goto-char (point-min))
-        (insert modeline "\n")))))
+    (ylsp-operations modeline)))
 
 (defun ylsp-k8s-modeline (name version)
   "Inserts the kubernetes modeline at the top of the buffer"
   (interactive "MName of k8s Kind: ")
   (let* ((url "https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.15.7-standalone-strict/")
-        (modeline (concat ylsp-modeline url name)))
-    (save-excursion
-      (save-restriction
-        (widen)
-        (goto-char (point-min))
-        (insert modeline "\n")))))
+         (modeline (concat ylsp-modeline url name)))
+    (ylsp-operations modeline)))
 
 (defun ylsp-generic-modeline (url)
   "Inserts a yaml schema modeline using url"
   (interactive "MUrl that contains the schema: ")
   (let ((modeline (concat ylsp-modeline url)))
-    (save-excursion
-      (save-restriction
-        (widen)
-        (goto-char (point-min))
-        (insert modeline "\n")))))    
+    (ylsp-operations modeline)))
 ;;
