4b68628da6616450db28d21dad5434e83074c3b9
Cesar Lizarraga <calizarr@gmail.com>
2023-10-29 20:36:14 -0500

    Add k8s-mode snippet and configure k8s-mode

--- modules/infrastructure/k8s-conf.el
+++ modules/infrastructure/k8s-conf.el
@@ -1,10 +1,10 @@
 (use-package k8s-mode
   :ensure t
   :hook (k8s-mode . yas-minor-mode)
-  :config (setq k8s-indent-offset nil
+  :config (setq k8s-indent-offset 2
                 k8s-site-docs-url "https://kubernetes.io/docs/reference/generated/kubernetes-api/"
                 k8s-search-documentation-browser-function (quote chrome)
-                k8s-site-docs-version "v1.15"))
+                k8s-site-docs-version "v1.25"))
 
 (use-package kubernetes
   :ensure t
